842d5d95f07593ef24bd239268ebfa717baf8f9f
wei kee haw <weikeehaw@weis-MacBook-Pro.local>
2019-05-23 21:16:52 +0800

    strings

---
+++ basics/strings.js
@@ -0,0 +1,12 @@
+let firstName = "Wei Kee";
+let lastName = "Haw";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// challenge 1
+let city, country, location;
+
+city = "Johor Bahru";
+country = "Malaysia";
+location = city + ", " + country;
+console.log(location);
